c0147c6fb60d39874112b8738061616e06b4e08e
aciden <aciden@septempus.ru>
2016-03-14 13:56:53 +0300

    test

--- ReceivData.php
+++ ReceivData.php
@@ -5,21 +5,44 @@ namespace aciden\receivData;
 use aciden\receivData\FtpUpload;
 use aciden\receivData\FetchDataFile;
 
+/**
+ * 
+ */
 class ReceivData extends \yii\base\Component
 {
+    /**
+     *
+     * @var type 
+     */
     public $ftpPasv = true;
     public $clear = true;
 
+    /**
+     *
+     * @var type 
+     */
     private $_uploadFilePath = '';
     private $_loadFile;
     private $_fetchData;
 
+    /**
+     * 
+     * @param type $fileName
+     * @param type $uploadFilePath
+     */
     public function initialise($fileName, $uploadFilePath = null)
     {        
         $this->_loadFile = $fileName;
         $this->_uploadFilePath = $uploadFilePath ? $uploadFilePath : $this->_uploadFilePath;
     }
 
+    /**
+     * 
+     * @param type $host
+     * @param type $login
+     * @param type $pas
+     * @param type $ftpPath
+     */
     public function uploadFtp($host, $login, $pas, $ftpPath)
     {
         $fileUpload = new FtpUpload($host, $login, $pas, $this->ftpPasv);
@@ -27,13 +50,26 @@ class ReceivData extends \yii\base\Component
         
     }
     
-    public function fecthPreload($startLine, $numLoadLine, $separator = null)
+    /**
+     * 
+     * @param type $startLine
+     * @param type $numLoadLine
+     * @param type $separator
+     * @return type
+     */
+    public function fetchPreload($startLine, $numLoadLine, $separator = null)
     {
         $this->_fetchData = new FetchDataFile($this->_uploadFilePath . '/' . $this->_loadFile, $startLine, $numLoadLine, $separator);
         
         return $this->_fetchData->getData();
     }
 
+    /**
+     * 
+     * @param type $startLine
+     * @param type $separator
+     * @return type
+     */
     public function fetchRow($startLine = 1, $separator = null)
     {
         $this->_fetchData = new FetchDataFile($this->_uploadFilePath . '/' . $this->_loadFile, $startLine, 20, $separator);
@@ -41,6 +77,13 @@ class ReceivData extends \yii\base\Component
         return $this->_fetchData->getData();
     }
     
+    /**
+     * 
+     * @param array $assoc
+     * @param type $startLine
+     * @param type $separator
+     * @return type
+     */
     public function fetchAssoc(array $assoc, $startLine = 1, $separator = null)
     {
         $this->_fetchData = new FetchDataFile($this->_uploadFilePath . '/' . $this->_loadFile, $startLine, 0, $separator, $assoc);
@@ -48,6 +91,9 @@ class ReceivData extends \yii\base\Component
         return $this->_fetchData->getData();
     }
     
+    /**
+     * 
+     */
     public function __destruct()
     {        
         if ($this->clear && file_exists(__DIR__ . '/' . $this->_uploadFilePath . '/' . $this->_loadFile)) {
